520197b59c4c9290230a6b4f4ab74b9cc480cc51
shabazzm <shabazzm@oregonstate.edu>
2021-02-18 20:25:39 -0600

    Added ability for different photo views in landscape vs portrait

--- app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flickster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -70,7 +71,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+
+            //if phone is in landscape view
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                //then imageurl is backdrop image
+                imageUrl = movie.getBackdropPath();
+            }else{
+                // else should be poster image
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
 
         }
     }
--- app/src/main/java/com/example/flickster/models/Movie.java
+++ app/src/main/java/com/example/flickster/models/Movie.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 public class Movie {
     //class that encapsulates ideas of a movie
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
@@ -31,6 +33,9 @@ public class Movie {
 
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+    public String getBackdropPath(){
+        return String.format("https://image.tmdb.org/t/p/w342/%s",backdropPath);
     }
 
     public String getTitle() {
