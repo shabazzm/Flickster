27463f548f064660ccedfd78bb3314551b2e5e09
shabazzm <shabazzm@oregonstate.edu>
2021-02-23 20:28:29 -0600

    Working version of HW due friday. Allows user to click on movie and be taking to page showing title, trailer, overview and rating

--- app/src/main/java/com/example/flickster/DetailActivity.java
+++ app/src/main/java/com/example/flickster/DetailActivity.java
@@ -4,20 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flickster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeThumbnailView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 
-public class DetailActivity extends AppCompatActivity {
+import okhttp3.Headers;
+
+public class DetailActivity extends YouTubeBaseActivity {
+
+    private static final String YOUTUBE_API_KEY = "AIzaSyDLDavOZogTexZYBrmFWm193G6gmwV2kB4";
+    public static final String  VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
     TextView tvOverview;
     RatingBar ratingBar;
+    YouTubePlayerView youTubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +48,64 @@ public class DetailActivity extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.black));
         }
 
-        getSupportActionBar().hide();
+       // getSupportActionBar().hide();
 
         tvTitle = findViewById(R.id.tvTitle);
         tvOverview = findViewById(R.id.tvOverview);
         ratingBar = findViewById(R.id.ratingBar);
+        youTubePlayerView = findViewById(R.id.player);
 
 
         Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
         ratingBar.setRating((float)movie.getRating());
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(String.format(VIDEOS_URL, movie.getMovieId()), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                try {
+                    JSONArray results = json.jsonObject.getJSONArray("results");
+
+                    if (results.length() == 0){
+                        return;
+
+                    }
+                    String youTubeKey = results.getJSONObject(0).getString("key");
+                    Log.d("DetailActivity", youTubeKey);
+                    initializeYoutube(youTubeKey);
+
+                } catch (JSONException e) {
+                    Log.e("DetailActivity", "Failed to parse",e);
+
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+
+            }
+        });
+
+    }
+
+    private void initializeYoutube(final String youTubeKey) {
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d("Detail Activity", "onInitializationSuccess");
+                // do any work here to cue video, play video, etc.
+                youTubePlayer.cueVideo(youTubeKey);
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d("DetailActivity", "onInitializationSuccess");
+            }
+        });
 
     }
 
